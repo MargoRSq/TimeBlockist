83a28e705aa8d29c874b6d9aa47f0ca56f304e90
margosq <slava1kvartovkin@gmail.com>
2022-02-21 18:41:50 +0300

    fix: args input

--- main.py
+++ main.py
@@ -5,16 +5,18 @@ from src.exc import invalid_arguments_amount
 from src.timeblockist import set_priorities
 
 def main():
-    if ((len(av) - 1) % 4) != 0:
+    av_len = len(av)
+    av_len_range = range(0, av_len)
+    if ((av_len - 1) % 4) != 0:
         raise invalid_arguments_amount("\nEnter 4 arguments:\n1)Label\n2,3,4)Time segment for BLUE, YELLOW and RED priorities")
 
     while (1):
-        for i in range(1, (len(av) - 1) // 4 + 1):
-            mn = 0 if i == 1 else 3
-            set_priorities(av[(i + mn)],
-                           av[(i + mn) + 1],
-                           av[(i + mn) + 2],
-                           av[(i + mn) + 3])
+        for i in av_len_range[1::4]:
+            label = av[i]
+            blue = av[i + 1]
+            yellow = av[i + 2]
+            red = av[i + 3]
+            set_priorities(label, blue, yellow, red)
         sleep(30)
 
 if __name__ == '__main__':
